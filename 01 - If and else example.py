4983dbe43a49b94582171154fb730fb48e4d1e30
Keshab Kumar Pathak <156252352+Keshab0310@users.noreply.github.com>
2024-02-28 21:44:54 -0500

    Add files via upload

---
+++ 01 - If and else example.py
@@ -0,0 +1,7 @@
+print("Welcome to the Rollercoster!!")
+height = int(input("Enter your height in cm: "))
+
+if height >= 120:     #This  is a condition that checks if the user's inputted height is greater than 120 cm or equal to 120.
+    print("You can ride the Rollercoster") #If it is, then this message will be printed
+else:               #Otherwise, if the user's inputted height is not greater than 120
+    print("Sorry, you are too short. You cannot ride the Rollercoster.") #Then this message will be printed. 
